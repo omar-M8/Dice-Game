1e90f61a57ee9d40b87e7f824d9bb328a8ec1131
Omar <mohammed.omar078@gmail.com>
2025-11-25 14:22:56 -0800

    Instantiate Dice objects for both player and computer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 
-const playerTotalScore = 0;
-const computerTotalScore = 0;
-
 //Dice object
-export class Dice {
+class Dice {
     constructor(minValue, maxValue) {
         this.minValue = minValue;
         this.maxValue = maxValue;
@@ -18,4 +15,34 @@ export class Dice {
 const d6 = new Dice(1, 6);
 console.log("Rolling a 6-sided die:");
 console.log(d6.roll());
-console.log(d6.roll());
+
+function rollAllDices() {
+
+}
+
+const totalPlayerScore = 0;
+const totalComputerScore = 0;
+
+let player1dice1 = new Dice(1, 6);
+let player1dice2 = new Dice(1, 6);
+
+let computerDice1 = new Dice(1, 6);
+let computerDice2 = new Dice(1, 6);
+
+
+
+function startGame() {
+    console.log('Hello')
+
+    let player1stRoll = player1dice1.roll()
+    console.log('Player 1st Dice is: ' + player1stRoll)
+
+    let player2ndRoll = player1dice2.roll()
+    console.log(`Player 2nd Dice is: ${player2ndRoll}`)
+
+    let computer1stRoll = computerDice1.roll()
+    console.log(`Player 2nd Dice is: ${computer1stRoll}`)
+
+    let computer2ndRoll = computerDice1.roll()
+    console.log(`Player 2nd Dice is: ${computer2ndRoll}`)
+}
